b8ecee1a7586818133aad2b737e80a7afa9a9205
winson <angihu@126.com>
2019-03-21 11:13:46 +0800

    Initial Commit

---
+++ testGit2/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  testGit2
+//
+//  Created by winsonGali on 2019/3/21.
+//  Copyright © 2019 winsonGali. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ testGit2Tests/testGit2Tests.m
@@ -0,0 +1,37 @@
+//
+//  testGit2Tests.m
+//  testGit2Tests
+//
+//  Created by winsonGali on 2019/3/21.
+//  Copyright © 2019 winsonGali. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface testGit2Tests : XCTestCase
+
+@end
+
+@implementation testGit2Tests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
